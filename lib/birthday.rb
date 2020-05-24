d0fed8f927d02d628ac07aa5defd0adb85ef0286
Zana H <zhertica@gmail.com>
2020-05-24 03:34:31 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,5 +5,8 @@
 # }
 
 def happy_birthday(birthday_kids)
-  # add your code snippet here!
+  happy_birthday(birthday_kids)
+  birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
 end
